73ed5f6022f9fd5256adf6f1c1cfc1ba8b938569
algarv <annagarverick2022@u.northwestern.edu>
2022-05-15 20:58:23 -0500

    Added calibration node

--- talker_listener/nodes/QC_node3.py
+++ talker_listener/nodes/QC_node3.py
@@ -2,50 +2,76 @@
 
 import rospy
 import random
-from std_msgs.msg import String, Float64, Float64MultiArray
+import numpy as np
+from std_msgs.msg import Float64, Float64MultiArray
 from talker_listener.qc_predict import MUdecomposer
 from rospy.core import logdebug
 
 path = rospy.get_param("/file_dir")
 #model_file = path + "\\src\\talker_listener\\best_model_cnn-allrun5_c8b_mix4-SG0-ST20-WS40-MU[0, 1, 2, 3]_1644222946_f.h5"
-model_file = path + "/src/talker_listener/best_model_cnn-allrun5_c8b_mix4-SG0-ST20-WS40-MU[0, 1, 2, 3]_1644222946_f.h5"
+model_file = path + "/src/talker_listener/" + "best_model_cnn-allrun5_c8b_mix4-SG0-ST20-WS40-MU[0, 1, 2, 3]_1644222946_f.h5"
 model = MUdecomposer(model_file)
-sample = [] 
-def predict_MUs(hdEMG):
-    logdebug(hdEMG.data)
-    global neural_drive
-    if len(sample) < 124:
+
+win = 40
+method = 'emg' # 'cst'
+
+def get_sample(hdEMG):
+    global sample 
+    
+    if len(sample) < win:
         sample.append(hdEMG.data)
     else:
         sample.pop()
         sample.append(hdEMG.data)
-        neural_drive = model.predict_MUs(hdEMG.data)
 
-    # neural_drive = random.randint(0,1) 
+def calc_torque_cst():
+    global sample
 
-def calc_torque(neural_drive):
+    nueral_drive = model.predict_MUs(sample)
     #Linear regression to map neural drive to torque
+
     return random.randint(-10,10)
+
+def calc_torque_emg():
+    global sample
+    m = rospy.get_param('slope')
+    b = rospy.get_param('intercept')
+    try: 
+        torque_cmd = m * sample + b #need to take specific sample value
+    except:
+        logdebug("Waiting for sample")
+        torque_cmd = 0
+    
+    return torque_cmd
 
 def main():
     rospy.init_node('QC_node', log_level=rospy.DEBUG)
-    r = rospy.Rate(100) #100Hz for torque controller
-
+    r = rospy.Rate(10) #100Hz for torque controller
     pub = rospy.Publisher('/h3/right_ankle_effort_controller/command', Float64, queue_size=10)
-    sub = rospy.Subscriber('hdEMG_stream', Float64MultiArray, predict_MUs)
-
+    sub = rospy.Subscriber('hdEMG_stream', Float64MultiArray, get_sample)
+    first_run = True
     while not rospy.is_shutdown():
+        global sample
+        sample = []
+        if rospy.get_param('calibrated') == True:
+            if first_run == True:
+                sample = []
+            first_run = False
+            try:
+                if method == 'cst':
+                    torque_cmd = calc_torque_cst()
+                
+                if method == 'emg':
+                    torque_cmd = 10*calc_torque_emg()
+            
+                logdebug("Torque_CMD: ")
+                logdebug(torque_cmd)
+            except:
+                logdebug("Could not find torque_cmd")
+                continue
 
-        global neural_drive
-        try:
-            torque_cmd = calc_torque(neural_drive)
-            # logdebug(torque_cmd)
-        except:
-            # logdebug("Could not find torque_cmd")
-            continue
-
-        pub.publish(torque_cmd)
-        r.sleep()
+            pub.publish(torque_cmd)
+            r.sleep()
 
 if __name__ == '__main__':
     try:
--- talker_listener/nodes/calibrate.py
+++ talker_listener/nodes/calibrate.py
@@ -1,22 +1,51 @@
 #!/usr/bin/env python
 
-import rospy
-from std_srvs.srv import Empty
-from State.msg import joint_motor_torque, joint_torque_sensor
+import rospy 
+import numpy as np
+from std_msgs.msg import Float64, Float64MultiArray
+from sklearn import linear_model as lm
+from rospy.core import logdebug
 
-sub = rospy.Subscriber('/h3/robot_states/joint_torque_sensor[5]', Float64MultiArray, calib)
-torque_array = []
 max_torque = 0
+trial_length = 60 #seconds
 
-def calib(torque):
+def torque_calib(sensor_reading):
+    global torque_array
+    torque = sensor_reading.data[5]
     torque_array.append(torque)
     if torque > max_torque:
         rospy.set_param('Max_Torque', torque)
 
+def emg_calib(hdEMG):
+    global emg_array
+    emg_array.append(hdEMG.data) #pick specific value?
+    
 def main():
     rospy.init_node('calibration', log_level=rospy.DEBUG)
-    # calibrate = rospy.service('calibrate','calibration.srv',calib)
-    return 0
+    torque_sub = rospy.Subscriber('/h3/robot_states/joint_torque_sensor', Float64MultiArray, torque_calib)
+    emg_sub = rospy.Subscriber('hdEMG_stream', Float64MultiArray, emg_calib)
+    r = rospy.Rate(1024)
+    
+    global torque_array
+    global emg_array
+    torque_array = []
+    emg_array = []
+
+    start = rospy.Time.now()
+    duration = rospy.Duration.from_sec(trial_length) 
+    while (rospy.Time.now() < start + duration):
+        logdebug(torque_array)
+        r.sleep()
+
+    y = np.array(torque_array)
+    x = np.array(emg_array)
+    model = lm.LinearRegression
+    res=model.fit(x,y)
+    intercept = res.params[0]
+    slope = res.params[1]
+    rospy.set_param('Intercept',intercept)
+    rospy.set_param('Slope',slope)
+    rospy.set_param('calibrated', True)
 
 if __name__ == '__main__':
     try:
